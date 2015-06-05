14986eb64d456f65dcf6b0cb6ae5752e13ba2884
Raymond Morcos <remongeorge@live.com>
2015-06-05 00:19:59 -0700

    adding-users controller-user edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
 
-  get 'welcome/about'
-
-  root to: 'welcome#index'
-
-  
+  	devise_for :users
+	resources :users, only: [:update, :show, :index]			
+ 	get 'welcome/about'
+	root to: 'welcome#index'
 end
